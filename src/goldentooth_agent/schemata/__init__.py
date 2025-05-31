d72e61abb668733ad46a096c5a8c0aaad42bb9e1
Nate Douglas <ndouglas@users.noreply.github.com>
2025-05-31 14:58:30 -0400

    Adds schemata module.

---
+++ src/goldentooth_agent/schemata/__init__.py
@@ -0,0 +1,11 @@
+import importlib
+import pkgutil
+import sys
+
+def import_all_schemata():
+  package = sys.modules[__name__]
+  for _, modname, ispkg in pkgutil.iter_modules(package.__path__):
+    if not ispkg:
+      importlib.import_module(f"{package.__name__}.{modname}")
+
+import_all_schemata()
