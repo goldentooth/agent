a7ac6a59f1ba60677ab52edb690dd950a8b6bc9f
Nate Douglas <ndouglas@users.noreply.github.com>
2025-05-31 17:48:07 -0400

    General updates.

---
+++ src/goldentooth_agent/__init__.py
@@ -0,0 +1,18 @@
+from . import (
+  agent, chat, context_provider, context_providers,
+  greeting, initial_context, schemata, system_prompt,
+  tool, tools
+)
+
+__all__ = [
+  "agent",
+  "chat",
+  "context_provider",
+  "context_providers",
+  "greeting",
+  "initial_context",
+  "schemata",
+  "system_prompt",
+  "tool",
+  "tools",
+]
--- src/goldentooth_agent/cli.py
+++ src/goldentooth_agent/cli.py
@@ -1,8 +1,9 @@
 import asyncio
 import typer
 from dotenv import load_dotenv
-from .chat_session import ChatSession
+from .chat import ChatSession
 from .initial_context import InitialContext
+from .system_prompt import SystemPromptFactory
 
 load_dotenv()
 app = typer.Typer()
@@ -13,7 +14,11 @@ def chat():
   Start a chat session
   """
   initial_context = InitialContext()
-  chat_session = ChatSession(initial_context)
+  system_prompt_generator = SystemPromptFactory.get()
+  chat_session = ChatSession(
+    initial_context=initial_context,
+    system_prompt_generator=system_prompt_generator,
+  )
   asyncio.run(chat_session.start())
 
 @app.command()
