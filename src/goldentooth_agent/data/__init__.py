8e732be1f738213af829e3573b4d0c6da7bb8430
Nate Douglas <ndouglas@users.noreply.github.com>
2025-06-24 10:00:59 -0400

    Rename some files.

--- src/goldentooth_agent/data/__init__.py
+++ src/goldentooth_agent/data/__init__.py
@@ -1,8 +1,9 @@
-from . import context_providers, system_prompts, personas, roles
+from . import context_providers, system_prompts, personas, roles, scenarios
 
 __all__ = [
   "context_providers",
   "system_prompts",
   "personas",
   "roles",
+  "scenarios",
 ]
